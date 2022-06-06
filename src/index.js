fd2f1e16974514255a679d2323f33810f1484397
junaidjahan <junaid.jahan@itcurves.com>
2022-06-06 20:06:10 +0500

    base route

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <RecoilRoot>
-    <BrowserRouter>
+    <BrowserRouter basename="https://junaidjahan.github.io/rev-bot/">
       <App />
     </BrowserRouter>
   </RecoilRoot>,
